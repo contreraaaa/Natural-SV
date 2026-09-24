fae1da464c5ebed7ccffb2aff31dd638af42ebdc
Danielrodas1 <cerinrodas@gmail.com>
2026-09-24 17:07:30 -0600

    Arregle un pequeño error que hacia que no cargara la pagina

--- src/components/NaturalSVApp.tsx
+++ src/components/NaturalSVApp.tsx
@@ -462,7 +462,7 @@
             <ChevronRight size={18} />
           </Link>
         </div>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4" lg:grid-cols-4>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
           {[
             {
               label: "Ventas entregadas",
